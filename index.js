4110067dbc69e2e51f7238619e826afa9ca0e7bb
Ashish Bishnoi <bishnoi11011@gmail.com>
2026-03-18 15:37:26 +0530

    add express

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("code is running");
+const express = require("express");
+const app = express();
+
+const USERS_DATA = [
+  {
+    userName: "Ashish",
+    age: 42,
+    email: "Xm1j9@example.com",
+  },
+  {
+    userName: "Rahul",
+    age: 42,
+    email: "Xm1j9@example.com",
+  },
+  {
+    userName: "Vishal",
+    age: 42,
+    email: "Xm1j9@example.com",
+  },
+  {
+    userName: "Aman",
+    age: 42,
+    email: "Xm1j9@example.com",
+  },
+  {
+    userName: "Aman",
+    age: 42,
+    email: "Xm1j9@example.com",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json(USERS_DATA);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
